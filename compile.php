37e0e0515b78274d08de0dfd6afea033e2b26460
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-02-07 23:46:52 +0300

    fixed named automatic builds fixed bugs in compile

--- compile.php
+++ compile.php
@@ -9,7 +9,7 @@ function sbversion()
     $build = file_get_contents('build');
     $build++;
     file_put_contents('build', $build);
-    $sb = round($build / 44.1, 3);
+    $sb = round($build / ($build * 0.441), 3);
     $manifest = file_get_contents('source/manifest.json');
     $manifest = (array)json_decode($manifest);
     $manifest['version'] = "$sb";
@@ -29,9 +29,8 @@ try {
     foreach ($minnerJson as $fileName => $data) {
 
         $results = [];
+        $results['jquery.min.js'] = file_get_contents('source/src/jquery.min.js');
         $fileName = 'source/js/' . $fileName . '.min.js';
-        $results['jquery.min.js'] = file_get_contents('header.js') .
-                                file_get_contents('source/src/jquery.min.js');
 
         foreach ($data as $file) {
             $path = 'source/src/' . $file;
@@ -42,7 +41,10 @@ try {
             }
             $results[$file] = $caches[$path];
         }
-        $data = str_replace("\n", "", implode(';', $results));
+        $minifier = new Minify\JS();
+        $minifier->add(str_replace(PHP_EOL, "", implode(';', $results)));
+        $data = file_get_contents('header.js') . $minifier->minify();
+        $data = str_replace(";;", ";", $data);
         $data = str_replace("{version}", $sbversion, $data);
         $data = str_replace("{year}", 2013 == date('Y') ? date('Y') : '2013 - ' . date('Y'), $data);
         file_put_contents($fileName, $data);
