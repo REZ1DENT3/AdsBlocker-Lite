06b758f4b1951a126fd9fa9a05a77c4ca4014237
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-02-09 17:01:24 +0300

    blockerRulesCount++; ++youtube

--- src/js/content.js
+++ src/js/content.js
@@ -2,6 +2,7 @@
     function monitorBlocks() {
         window.addEventListener("message", function receiveMessage(event) {
             if (event.data.type && (event.data.type == "blockedWindow")) {
+                blockerRulesCount++;
                 chrome.runtime.sendMessage({
                     name: "trackEvent",
                     action: "PopupBlocked",
