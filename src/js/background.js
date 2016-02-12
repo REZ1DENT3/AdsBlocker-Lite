e947768c0b854d5641f7b1eb9f6bf97ab57701ff
REZ1DENT3 <maksim.babichev95@gmail.com>
2016-02-12 11:57:06 +0300

    fixed

--- src/js/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.name == "setBadgeText") {
-        chrome.browserAction.setBadgeText({
-            text: request.text,
-            tabId: sender.tab.id
-        });
-    }
-});
